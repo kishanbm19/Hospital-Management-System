f398bef2edc3fb6238aadf7fd335836a8000bb3e
kishanbm19 <kbm.kishan19engg@gmail.com>
2026-05-16 17:46:25 +0530

    fixed allowed hosts

--- hospital_mgmt/settings.py
+++ hospital_mgmt/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-&1)8d%w*ac*my#&mgtt8p5jc(e$t*$p@v3v)49h(fxm(gx@3rb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["medicore-epj3.onrender.com", "localhost", "127.0.0.1"]
 
 # Application definition
 
